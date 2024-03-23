7b8cda85c58ee3e6def22ba5eb2b934e965d4f1b
Monty <41972631+Mr-Monty999@users.noreply.github.com>
2024-03-24 02:09:12 +0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mysql_user='root'
+files_source="/var/www"
+backup_folder="$(pwd)"
+database_folder="databases"
+name_prefix="$(date +%Y-%m-%d-%H%M%S)-backup"
+backup_file_name="${name_prefix}.tar.gz"
+max_backups=4
+
+### create required directories ###
+mkdir -p "${backup_folder}" "${backup_folder}/${database_folder}"
+mkdir -p "${backup_folder}/${name_prefix}"
+cd "${backup_folder}"
+
+### backup all databases ###
+databases=$(mysql -u $mysql_user -e "SHOW DATABASES")
+
+for db in $databases; do
+   mysqldump -u $mysql_user $db > "${database_folder}/${db}.sql" 2> /dev/null 
+done
+
+### arhive & compress all files and databases ###
+tar -cvzf "files.tar.gz" "${files_source}"
+mv "files.tar.gz" "${name_prefix}"
+mv "${database_folder}" "${name_prefix}"
+tar -cvzf "${backup_file_name}" "${name_prefix}"
+rm -rf "${name_prefix}"
+
+
+### delete old backups ###
+number_of_backups=$(ls | wc -w)
+if [ $number_of_backups -gt $max_backups ]; then
+oldest_backup=$(ls -htr | head -n 1)
+rm -rf $oldest_backup
+fi
